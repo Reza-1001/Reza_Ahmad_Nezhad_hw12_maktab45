bfcaed7f2c0880cae31d990e673d00379f3453ea
Reza Ahmad Nezhad <resistance1271@gmail.com>
2021-02-06 20:29:54 +0330

    hw12-7-Done

--- NodeJs/hw12-7/app.js
+++ NodeJs/hw12-7/app.js
@@ -1,44 +1,71 @@
-const http = require('http');
-const fs = require('fs');
+const http = require("http");
+const fs = require("fs");
+const cors = require("cors");
 
-http.createServer(function(request, response) {
+cors();
+http
+  .createServer(function (request, response) {
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Request-Method", "*");
+    response.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+    response.setHeader("Access-Control-Allow-Headers", "*");
     if (request.url === "/" && request.method === "GET") {
-        response.setHeader('Content-Type', 'text/html');
-        fs.readFile('./public/index.html', 'utf8', (err, page) => {
-            response.write(page);
-            response.end();
-        });
-    } 
-    else if (request.url === "/js/login.js" && request.method === "GET") {
-        fs.readFile('./public/js/login.js', 'utf8', (err, page) => {
-            response.setHeader('Content-Type', 'text/javascript');
-            response.write(page);
-            response.end();
-            
-        });
-    } else if (request.url === "/css/style.css" && request.method === "GET") {
-        fs.readFile('./public/css/style.css', 'utf8', (err, page) => {
-            response.setHeader('Content-Type', "text/css");
-            response.write(page);
-            response.end();
-        });
-    } else if (request.url === "/Pics/space.jpg" && request.method === "GET") {
-        fs.readFile('./public/Pics/space.jpg', (err, image) => {
-            response.setHeader('Content-Type','image/jpeg');
-            response.write(image);
-            response.end();
-            
-        });
-    }else if (request.url === "/validate" && request.method === "POST") {
-        response.writeHead(200, { 'Content-Type': 'text/plain' });
-        request.on('data', function (chunk) {
-        console.log('GOT DATA!');
-    });
-    response.end('callback(\'{\"msg\": \"OK\"}\')');
-    } else {
-        response.write("Bad Request");
+      response.setHeader("Content-Type", "text/html");
+      fs.readFile("./public/index.html", "utf8", (err, page) => {
+        response.write(page);
         response.end();
+      });
+    } else if (request.url === "/js/login.js" && request.method === "GET") {
+      fs.readFile("./public/js/login.js", "utf8", (err, page) => {
+        response.setHeader("Content-Type", "text/javascript");
+        response.write(page);
+        response.end();
+      });
+    } else if (request.url === "/css/style.css" && request.method === "GET") {
+      fs.readFile("./public/css/style.css", "utf8", (err, page) => {
+        response.setHeader("Content-Type", "text/css");
+        response.write(page);
+        response.end();
+      });
+    } else if (request.url === "/Pics/space.jpg" && request.method === "GET") {
+      fs.readFile("./public/Pics/space.jpg", (err, image) => {
+        response.setHeader("Content-Type", "image/jpeg");
+        response.write(image);
+        response.end();
+      });
+    } else if (request.method === "POST") {
+      let body = "";
+      request.on("data", (chunk) => {
+        body += chunk.toString();
+        body = JSON.parse(body);
+      });
+      let found = false;
+      request.on("end", () => {
+        fs.readFile("./users.json", "utf8", function (err, data) {
+          for (let i of JSON.parse(data)) {
+            if (i.username === body.username && i.password === body.password) {
+              found = true;
+              break;
+            } else found = false;
+          }
+          
+          if (found) {
+            response.writeHead(200, { "Content-Type": "text/plain" });
+
+            response.write(JSON.stringify(1));
+            response.end();
+          } else {
+            response.writeHead(200, { "Content-Type": "text/plain" });
+            response.write(JSON.stringify(0));
+            response.end();
+          }
+        });
+      });
+    } else {
+      response.write("Bad Request");
+      response.end();
     }
-}).listen(5005);
+  })
+  .listen(5005);
 
 console.log("Server started on port 5005 ...");
--- NodeJs/hw12-7/public/js/login.js
+++ NodeJs/hw12-7/public/js/login.js
@@ -2,16 +2,36 @@ $(document).ready(() => {
   $("#submit").on("click", () => {
     if ($("#name").val().trim() === "" || $("#pass").val().trim() == "") {
       alert("Fill in user and pass");
-    }
-    else{
+    } else {
+      let myBody = {
+        userName: $("#name").val(),
+        password: $("#pass").val(),
+      };
+      myBody = JSON.stringify(myBody);
+
       $.ajax({
         method: "POST",
-        url: "/validate",
-        data:'{"reza","reza"}',
+        url: "http://127.0.0.1:5005/",
+        contentType: "application/json",
+        data: myBody,
         success: function (response) {
-          
+          if (+response === 1) {
+            alert("ورود موفقیت آمیز");
+          } else {
+            alert("کاربری با این مشخصات وجود ندارد");
+            $("#name").css("border", "2px solid red");
+            $("#name").css("color", "red");
+            $("#pass").css("border", "2px solid red");
+            $("#pass").css("color", "red");
+          }
         },
       });
     }
+  });
+  $("#name").on("change", () => {
+    $("#name").css("border", "1px solid black");
+    $("#name").css("color", "black");
+    $("#pass").css("border", "1px solid black");
+    $("#pass").css("color", "black");
   });
 });
